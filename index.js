00c7ad99aa1163276aa8b2d6387ee1455c3ee532
Daryl Tan Chin Ting <daryltanct@gmail.com>
2021-03-08 20:33:19 +0000

    :recycle: extract some operations into separate functions

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express')
 const app = express()
 const httpServer = require('http').createServer(app)
 const io = require('socket.io')(httpServer)
-const uniqid = require('uniqid')
 
 const connectDB = require('./db')
-const { closeRoom } = require('./utils/helper')
+const { attemptReconnect, startGame, swapColor, disconnectProcess } = require('./utils/helper')
 
 // connect database
 connectDB()
@@ -19,78 +18,25 @@ app.use('/api/auth', require('./routes/auth'))
 let playerQueue = []
 let activeRooms = []
 
-const startGame = () => {
-	const player1 = playerQueue.shift()
-	const player2 = playerQueue.shift()
-
-	// prevent same account from playing with each other
-	if (player1.playerId === player2.playerId) {
-		playerQueue.push(player1)
-		playerQueue.push(player2)
-		return
-	}
-
-	const coinFlip = Math.random() > 0.5
-	const whitePlayer = coinFlip ? player1 : player2
-	const blackPlayer = coinFlip ? player2 : player1
-
-	whitePlayer.color = 'white'
-	blackPlayer.color = 'black'
-
-	const roomId = uniqid() // generate random room id
-	whitePlayer.join(roomId)
-	blackPlayer.join(roomId)
-
-	// add room to active rooms
-	activeRooms.push({
-		roomId,
-		players: [ whitePlayer.playerId, blackPlayer.playerId ],
-		white: {
-			playerId: whitePlayer.playerId,
-			isActive: true
-		},
-		black: {
-			playerId: blackPlayer.playerId,
-			isActive: true
-		}
-	})
-
-	whitePlayer.emit('gameStart', { color: 'white', roomId: roomId, opponent: { id: blackPlayer.id, rematch: false } })
-	blackPlayer.emit('gameStart', { color: 'black', roomId: roomId, opponent: { id: whitePlayer.id, rematch: false } })
-}
-
 io.on('connection', (socket) => {
 	console.log(`Socket ${socket.id} has connected`)
 	socket.playerId = socket.handshake.query.playerId // attach playerId to socket
 
-	// check if player is attempting to reconnect to an active room
-	const idx = activeRooms.findIndex((room) => room.players.includes(socket.playerId))
-	// if room exists:
-	if (idx !== -1) {
-		const recRoom = activeRooms[idx]
-		const myColor = recRoom.white.playerId === socket.playerId ? 'white' : 'black' // check color of player
-		socket.color = myColor // attach color to socket
-		socket.join(recRoom.roomId) // re-join room
-		// return to client
-		socket.emit('reconnect', {
-			roomId: recRoom.roomId,
-			pgn: recRoom.pgn, // to restore game state
-			color: myColor,
-			opponent: { id: myColor === 'white' ? recRoom.black.playerId : recRoom.white.playerId, rematch: false }
-		})
-		activeRooms[idx][myColor].isActive = true // reset isActive to true
-	}
+	attemptReconnect(socket, activeRooms)
 
 	socket.on('findGame', (signal) => {
 		if (signal) {
 			playerQueue.push(socket)
 		} else {
-			playerQueue = playerQueue.filter((s) => socket.id !== s.id)
+			// remove user from player queue
+			const playerIndex = playerQueue.findIndex((s) => s.id === socket.id)
+			playerQueue.splice(playerIndex, 1)
 		}
 
 		if (playerQueue.length >= 2) {
-			startGame()
+			startGame(playerQueue, activeRooms)
 		}
+		console.log(playerQueue.length)
 	})
 
 	socket.on('move', (data) => {
@@ -103,13 +49,7 @@ io.on('connection', (socket) => {
 	})
 
 	socket.on('gameEnd', (data) => {
-		// update active room: swap players' color
-		const roomIndex = activeRooms.findIndex((room) => room.roomId == data.roomId)
-		const temp = activeRooms[roomIndex].white.playerId
-		activeRooms[roomIndex].white.playerId = activeRooms[roomIndex].black.playerId
-		activeRooms[roomIndex].black.playerId = temp
-		// set pgn to empty state
-		activeRooms[roomIndex].pgn = ''
+		swapColor(data.roomId, activeRooms)
 
 		socket
 			.to(data.roomId)
@@ -128,26 +68,14 @@ io.on('connection', (socket) => {
 	})
 
 	socket.on('disconnecting', () => {
-		socket.rooms.forEach((room) => {
-			// check if user is in an active room
-			const roomIndex = activeRooms.findIndex((r) => r.roomId == room)
-			if (roomIndex !== -1) {
-				// if user is guest or other player is inactive, leave/close room
-				const oppColor = socket.color === 'white' ? 'black' : 'white'
-				if (!activeRooms[roomIndex][oppColor].isActive) {
-					closeRoom(room, activeRooms)
-				} else {
-					// if not guest, update active room: set isActive to false
-					activeRooms[roomIndex][socket.color].isActive = false
-				}
-			}
-			// console.log(activeRooms)
-			socket.to(room).emit('playerDisconnect', 'Opponent has disconnected')
-		})
+		disconnectProcess(socket, activeRooms)
 	})
 
 	socket.on('disconnect', () => {
-		playerQueue = playerQueue.filter((s) => socket.id !== s.id)
+		// remove user from player queue
+		const playerIndex = playerQueue.findIndex((s) => s.id === socket.id)
+		playerQueue.splice(playerIndex, 1)
+
 		console.log(`Socket ${socket.id} has disconnected`)
 	})
 })
--- utils/helper.js
+++ utils/helper.js
@@ -1,6 +1,98 @@
+const uniqid = require('uniqid')
+
+const attemptReconnect = (socket, activeRooms) => {
+	// check if player is in any active room
+	const roomIndex = activeRooms.findIndex((room) => room.players.includes(socket.playerId))
+	if (roomIndex === -1) return // no active room
+
+	const recRoom = activeRooms[roomIndex]
+	const myColor = recRoom.white.playerId === socket.playerId ? 'white' : 'black' // check color of player
+	socket.color = myColor // attach color to socket
+	socket.join(recRoom.roomId) // re-join room
+
+	socket.emit('reconnect', {
+		roomId: recRoom.roomId,
+		pgn: recRoom.pgn, // to restore game state
+		color: myColor,
+		opponent: { id: myColor === 'white' ? recRoom.black.playerId : recRoom.white.playerId, rematch: false }
+	})
+	activeRooms[roomIndex][myColor].isActive = true // reset isActive to true
+}
+
+const startGame = (playerQueue, activeRooms) => {
+	const player1 = playerQueue.shift()
+	const player2 = playerQueue.shift()
+
+	// prevent same account from playing with each other
+	if (player1.playerId === player2.playerId) {
+		playerQueue.push(player1)
+		playerQueue.push(player2)
+		return
+	}
+
+	const coinFlip = Math.random() > 0.5
+	const whitePlayer = coinFlip ? player1 : player2
+	const blackPlayer = coinFlip ? player2 : player1
+
+	whitePlayer.color = 'white'
+	blackPlayer.color = 'black'
+
+	const roomId = uniqid() // generate random room id
+	whitePlayer.join(roomId)
+	blackPlayer.join(roomId)
+
+	// add room to active rooms
+	activeRooms.push({
+		roomId,
+		players: [ whitePlayer.playerId, blackPlayer.playerId ],
+		white: {
+			playerId: whitePlayer.playerId,
+			isActive: true
+		},
+		black: {
+			playerId: blackPlayer.playerId,
+			isActive: true
+		}
+	})
+
+	whitePlayer.emit('gameStart', { color: 'white', roomId: roomId, opponent: { id: blackPlayer.id, rematch: false } })
+	blackPlayer.emit('gameStart', { color: 'black', roomId: roomId, opponent: { id: whitePlayer.id, rematch: false } })
+}
+
+const swapColor = (roomId, activeRooms) => {
+	const roomIndex = activeRooms.findIndex((room) => room.roomId == roomId)
+
+	const temp = activeRooms[roomIndex].white.playerId
+	activeRooms[roomIndex].white.playerId = activeRooms[roomIndex].black.playerId
+	activeRooms[roomIndex].black.playerId = temp
+
+	// set pgn to empty state
+	activeRooms[roomIndex].pgn = ''
+}
+
 const closeRoom = (roomId, roomsArr) => {
 	const idx = roomsArr.findIndex((room) => room.roomId == roomId)
 	roomsArr.splice(idx, 1)
 }
 
-module.exports = { closeRoom }
+const disconnectProcess = (socket, activeRooms) => {
+	// iterate through every room that socket is in
+	socket.rooms.forEach((room) => {
+		// check if room is active
+		const roomIndex = activeRooms.findIndex((r) => r.roomId == room)
+		if (roomIndex !== -1) {
+			// if user is guest or other player is inactive, leave/close room
+			const oppColor = socket.color === 'white' ? 'black' : 'white'
+			if (!activeRooms[roomIndex][oppColor].isActive) {
+				closeRoom(room, activeRooms)
+			} else {
+				// if not guest, update active room: set isActive to false
+				activeRooms[roomIndex][socket.color].isActive = false
+			}
+		}
+
+		socket.to(room).emit('playerDisconnect', 'Opponent has disconnected')
+	})
+}
+
+module.exports = { attemptReconnect, startGame, swapColor, closeRoom, disconnectProcess }
